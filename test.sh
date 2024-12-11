7cbba79603783526a03d4d97913861c4644c5ef2
Julian Nwadinobi <juliannwadinobi098@gmail.com>
2024-12-11 12:42:34 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,27 @@
 #!/bin/bash
 
-# Telegram Bot API Token and Chat ID
-TOKEN="your_telegram_bot_token"
-CHAT_ID="your_chat_id"
+# Telegram Bot API Token
+TOKEN="6766505665:AAEs3Y_PtHdjryOrFHcvL-g7MLTETxnyiKI"
+
+# Function to create hello.txt file
+create_file() {
+  echo "Hello, this is a message from your Telegram bot!" > hello.txt
+  echo "File hello.txt created."
+}
+
+# Use getUpdates API to retrieve chat_id from recent messages
+CHAT_ID=$(curl -s "https://api.telegram.org/bot$TOKEN/getUpdates" | jq -r '.result[0].message.chat.id')
+
+# Call the function to create the file
+create_file
 
 # Path to the file you want to send
-FILE_PATH="/path/to/your/file"
+FILE_PATH="hello.txt"
 
-# Send the file using Telegram Bot API
+# Send the file to the retrieved chat_id using the Telegram Bot API
 curl -X POST "https://api.telegram.org/bot$TOKEN/sendDocument" \
      -F chat_id="$CHAT_ID" \
      -F document=@"$FILE_PATH"
+
+# Clean up the hello.txt file after sending it
+rm hello.txt
